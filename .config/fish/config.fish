b75cc80f1c48eff10c17e6019e2c72c5ff8e1a84
Cameron <cameronwood321@gmail.com>
2025-04-21 15:04:07 +0100

    update fish config for go and default editor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
-fish_add_path $HOME/Applications
-fish_add_path /usr/local/go/bin
+fish_add_path $HOME/bin
 fish_add_path $HOME/go/bin
+fish_add_path /usr/local/go/bin
 
 alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias ls="eza"
@@ -9,6 +9,8 @@ alias la="eza --long --all"
 alias tree="eza --tree"
 alias cat="bat"
 alias g="git"
+
+set -gx EDITOR hx
 
 function fish_greeting
 end
